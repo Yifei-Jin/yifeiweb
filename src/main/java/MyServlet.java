4ecd02a8e424b0c1e18bc13d8677d4b4fb0ca413
Yifei-Jin <yj819@ic.ac.uk>
2021-11-25 13:16:51 +0000

    new

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/home"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
@@ -27,6 +28,9 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! "+reqBody);
     }
 
 }
